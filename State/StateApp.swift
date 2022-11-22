0a0cdea7fe1c21d93fce792eebb1a1de9051eb66
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2022-11-22 07:53:02 +0300

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Ian Nalyanya on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
